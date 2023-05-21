660b83197d1b86843e6c900ee92ca359afa8a87e
Jhelt <jonhelton405@gmail.com>
2023-05-21 01:09:56 -0400

    homeroutes file added in routes folder

---
+++ Develop/routes/homeRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  res.render('homepage')
+})
+
+router.get('/main', (req, res) => {
+  res.render('main')
+})
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+module.exports = router;
