82243f9bcce5e55ed04bfe462492155cc27c246d
Jhelt <jonhelton405@gmail.com>
2023-05-25 11:07:59 -0400

    need to render out handlebar pages in order to continue

--- Develop/routes/homeRoutes.js
+++ Develop/routes/homeRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-  res.render('homepage')
+  // res.render('homepage')
+  res.send('homepage')
 })
 
 router.get('/main', (req, res) => {
